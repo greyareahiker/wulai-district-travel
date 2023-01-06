d1ec7db33ab2d954e564b8ebde7a8704aad67f75
jereint20 <jereint20@gmail.com>
2023-01-07 00:38:10 +0900

    :sparkles: 기본 슬라이더 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+  swipeToSlide: true,
+  arrows: false,
+  infinite: false,
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider {...settings}>
+        <div className="slide-container">
+          <p>slider1</p>
+        </div>
+        <div className="slide-container">
+          <p>slider2</p>
+        </div>
+        <div className="slide-container">
+          <p>slider3</p>
+        </div>
+      </Slider>
     </div>
   );
 }
